511d74f25b09fa9e088f5cf4384306913f37abef
SHREYASH DHOTE <shreyashdhote362@gmail.com>
2026-06-02 22:42:16 +0530

    Task 10: Image gallery with filter

---
+++ task - 10/main.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.filter-btn');
+const items = document.querySelectorAll('.gallery-item');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        const filter = button.dataset.filter;
+
+        items.forEach(item => {
+            const category = item.dataset.category;
+
+            if (filter === 'all' || category === filter) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+});
